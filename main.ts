45e3535ebe2589829ed8e6b41b60271ee5958a19
Laiba3909 <160255993+Laiba3909@users.noreply.github.com>
2024-05-10 17:24:48 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,43 @@
+// console.log("Hello World"); //print
+// let favouriteColor = "blue"; // camel case
+//  let favourite_color = "blue";// snake case
+//  let FavouriteColor = "blue";// pascal case
+//  console.log(favouriteColor,favourite_color,FavouriteColor);
+// ctrl kc commmnet / cntrl ku uncomment
+ //strong typing
+// string number boolean
+//  let favourite_color: string ="laiba";
+//  let favouriteColor: number = 10;
+//  let FavouriteColor: boolean= true;
+//  let link: any= "https://docs.google.com/presentation/d/1mFtLVoMnXnSVtIRSr3GPCgIqgt-g_hgEqmV4qDBUxwg/edit#slide=id.g2ba847010a7_0_282";
+//  console.log(favouriteColor,favourite_color,FavouriteColor,link);
+
+ // let const diffwerence constant error:
+// let classNumber = 10;
+// const accountNumber= "12345567788"
+// classNumber = 50;
+// accountNumber = 10
+//   ERRORS
+// lett number = 10 // syntax error
+// console.log(number)
+// let name = "hello"
+// console.loger(name) // type error
+// let personName = "javeria" // assignbility error
+// personName = 10;
+// String Concetation
+// let firstName = "javeria";
+// let lastName = "laiba";
+// let fullName = firstName + '' + lastName;
+// console.log(fullName)
+
+let a = 2;
+ let b = 10;
+// console.log("before increement and decreement")
+// console.log(a)
+// console.log(b)
+// a++
+// b--
+// console.log("after increement and decreement")
+// console.log(a)
+// console.log(b)
+
